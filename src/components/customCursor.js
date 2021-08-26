a64808b3b343d8b3d4709e0b1d95b8cb91922382
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-08-26 13:13:21 +0530

    :tada: init(header and custom cursor)

---
+++ src/components/customCursor.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react"
+
+import { Cursor } from "../styles/globalStyles"
+
+// Context
+import { useGlobalStateContext } from "../context/globalContext"
+
+const CustomCursor = () => {
+  const { cursorType } = useGlobalStateContext()
+
+  const [mousePosition, setMousePosition] = useState({
+    x: 400,
+    y: 400,
+  })
+
+  const onMouseMove = event => {
+    const { pageX: x, pageY: y } = event
+    setMousePosition({ x, y })
+  }
+
+  useEffect(() => {
+    document.addEventListener("mousemove", onMouseMove)
+    return () => {
+      document.removeEventListener("mousemove", onMouseMove)
+    }
+  }, [])
+
+  return (
+    <>
+      <Cursor
+        className={`${!!cursorType ? "hovered" : ""} ${cursorType}`}
+        style={{ left: `${mousePosition.x}px`, top: `${mousePosition.y}px` }}
+      />
+    </>
+  )
+}
+
+export default CustomCursor
